b91053d3202979f2164d1b3948267b818a85c31b
John <emailjohnthomascaballero@gmail.com>
2023-08-29 18:43:56 +0800

    Day 1

---
+++ index.js
@@ -0,0 +1,64 @@
+// Conditional Statements
+
+// to run in terminal type "node index"
+// This tells Node.js to execute the index.js file.
+// let x = 10
+// let y = 6
+
+// if(x < 11){
+//   console.log("x < 11")
+// } else if (x == 9) {
+//   console.log("x == 9>")
+// } else {
+//   console.log("x > 11")
+// }
+
+// Switch Statement
+// const day = 'Wednesday';
+
+// switch (day) {
+//   case 'Monday':
+//     console.log("Its Monday");
+//     break;
+//   case 'Tuesday':
+//     console.log("Its Tuesday");
+//     break;
+//   case 'Wednesday':
+//     console.log("Its Wednesday");
+//     break;
+//   case 'Thursday':
+//     console.log("Its Thursday");
+//     break;
+//   case 'Friday':
+//     console.log("Its Friday");
+//     break;
+//   default:
+//     console.log("Enjoy the Weekend")
+//     break;
+// }
+
+// String Methods
+
+// The trim() method removes whitespace from both sides of a string:
+// let text1 = "    Hello World!     ";
+// console.log(text1.trim());
+
+// The replace() method replaces a specified value with another value in a string:
+// let text = 'Please learn Python'
+// console.log(text.replace('Python', 'JavaScript'));
+
+// Array
+let myList = []; //empty array
+let go = () => console.log("Hello World");
+
+myList = [
+  "banana",
+  3,
+  go,
+  ["John", "Lee"],
+  { firstname: "John", lastname: "Smith" },
+];
+
+console.log(myList[3][1]);
+console.log(myList[4].firstname);
+console.log(myList[2]);

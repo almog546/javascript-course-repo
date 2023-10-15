a8a6e191b1585c4dc14c6112b3af7fa279fb21f5
John <emailjohnthomascaballero@gmail.com>
2023-10-15 09:27:20 +0800

    Essential javascript concepts Day 2

---
+++ essential-javascript-concepts/operators.js
@@ -0,0 +1,39 @@
+// Operators:
+
+// Arithmetic Operators (+, -, *, /, %):
+// 1. Addition (+): The addition operator is used to add two numbers or concatenate two strings.
+let sum = 1 + 1;
+let message = "Hello, " + "World";
+console.log(sum + " " + message);
+
+
+
+// 2. Subtraction (-): The subtraction operator is used to subtract one number from another.
+let difference = 10 - 4;
+console.log(difference);
+
+
+
+// 3. Multiplication (*): The multiplication operator is used to multiply two numbers.
+let product = 5 * 5;
+console.log(product);
+
+
+
+// 4. Division (/): The division operator is used to divide one number by another.
+let quotient = 15 / 3;
+console.log(quotient);
+
+
+
+// 5. Modulus (%): The modulus operator returns the remainder of a division operation.
+let remainder = 17 % 5;
+console.log(remainder);
+
+
+
+
+
+
+// Comparison Operators (==, ===, !=, !==, >, <, >=, <=):
+// 1. Equality (==): The equality operator checks if two values are equal. It performs type coercion, so it may not be suitable for strict comparisons.

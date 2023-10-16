015f0665429e3d7205b801e158a60713a739b06c
John <emailjohnthomascaballero@gmail.com>
2023-10-16 17:33:25 +0800

    Essential javascript concepts Day 3

---
+++ essential-javascript-concepts/control-structures.js
@@ -0,0 +1,18 @@
+// Control Structures:
+// Conditional Statements (if, else if, else):
+// Conditional statements allow you to execute different blocks of code based on specified conditions. The primary conditional statements in JavaScript are if, else if, and else.
+
+// if statement:
+// The if statement executes a block of code if a specified condition is true.
+if ( 5 == 5) {
+  console.log('its equal to: 5');
+}
+
+
+// else if statement:
+// The else if statement allows you to specify additional conditions to test if the initial if condition is false.
+if (false) {
+  console.log('false');
+} else if (true) {
+  console.log('true');
+}

eaf0d624d4e9d3ae986103fee90b19c2f6a49634
John <emailjohnthomascaballero@gmail.com>
2023-10-20 10:24:05 +0800

    Essential javascript concepts Day 6

---
+++ essential-javascript-concepts/6-objects.js
@@ -0,0 +1,20 @@
+// Objects:
+// Creating Objects:
+// An object in JavaScript is a collection of key-value pairs. You can create objects using curly braces {} and populate them with properties and methods.
+let person = {}; // empty objects
+
+// objects with properties
+let book = {
+  title: "JavaScript for Beginner to Professional",
+  author: "Laurence Lars Svekis",
+  year: 2021
+};
+
+// Object Properties and Methods:
+// Objects can have properties (variables) and methods (functions) associated with them. You can access and manipulate these properties and methods using dot notation or square brackets.
+
+// Accessing object properties
+console.log(book.title); // Access the 'title' property
+console.log(book["author"]); // Access the 'author' property using square brackets
+
+// Adding a method to an object

674aa723ce0c0b7091c89b660f523251007c2c8a
John <emailjohnthomascaballero@gmail.com>
2023-10-31 16:38:16 +0800

    Essential javascript concepts Day 17

---
+++ essential-javascript-concepts/11-scope-and-closures.js
@@ -0,0 +1,10 @@
+Scope:
+// In JavaScript, scope determines the accessibility and visibility of variables and functions within your code. Understanding scope is essential for managing variable lifetimes and preventing naming conflicts.
+
+// Global Scope:
+// Variables declared outside of any function or block are in the global scope. They can be accessed from any part of your code.
+let globalVar = "I'm in global scope";
+
+function printGlobalVar() {
+  console.log(globalVar);
+}

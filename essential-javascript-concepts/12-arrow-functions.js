9661f185c301dfd5a721215c4a7dad6ba5943346
John <emailjohnthomascaballero@gmail.com>
2023-11-02 10:01:13 +0800

    Essential javascript concepts Day 19

---
+++ essential-javascript-concepts/12-arrow-functions.js
@@ -0,0 +1,6 @@
+// ES6+ Features:
+// Arrow Functions:
+// Arrow functions provide a concise way to write anonymous functions. They are often used for short, one-line functions and have a simpler syntax compared to traditional function expressions.
+
+// Syntax:
+const add = (a, b) => a + b;

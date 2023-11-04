aeb9edff1a649b93e30b76c56c9cf79616b7ce3c
John <emailjohnthomascaballero@gmail.com>
2023-11-04 20:43:57 +0800

    Essential javascript concepts Day 21

---
+++ essential-javascript-concepts/13-modules.js
@@ -0,0 +1,7 @@
+// Modules:
+// Modules are a way to organize and structure your JavaScript code into reusable pieces. They help you keep your code modular, maintainable, and organized by allowing you to break it into separate files or modules.
+
+// CommonJS Modules:
+// CommonJS is a module system used in Node.js. It uses the require and module.exports syntax to import and export modules.
+
+// Exporting with CommonJS:

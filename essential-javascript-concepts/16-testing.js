eef0e5abbb183e1924b5e72c08ff584aab5778ea
John <emailjohnthomascaballero@gmail.com>
2023-11-12 18:20:49 +0800

    Essential javascript concepts Day 28

--- essential-javascript-concepts/16-testing.js
+++ essential-javascript-concepts/16-testing.js
@@ -1,2 +1,11 @@
 // jest documentation: https://jestjs.io/docs/getting-started
 
+// Testing is an essential part of software development, and it helps ensure that your code functions correctly and reliably. In this beginner's guide, we'll focus on unit testing using the Jest testing framework, which is one of the most popular and beginner-friendly testing tools for JavaScript.
+
+// Unit Testing with Jest:
+// What is Unit Testing?
+// Unit testing is the practice of testing individual components or units of your code in isolation to ensure they work as expected. These units are typically functions or small pieces of code. The goal is to verify that each unit of your code behaves correctly.
+
+// Getting Started with Jest:
+// Jest is a widely-used testing framework for JavaScript. It's known for its simplicity and ease of use. To get started with Jest, follow these steps:
+

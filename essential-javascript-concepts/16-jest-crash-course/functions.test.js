c0bc85ac7b555406d52d0bdade6345f43687ea34
John <emailjohnthomascaballero@gmail.com>
2023-11-14 13:29:10 +0800

    Essential javascript concepts Day 30

--- essential-javascript-concepts/16-jest-crash-course/functions.test.js
+++ essential-javascript-concepts/16-jest-crash-course/functions.test.js
@@ -43,8 +43,10 @@ test("should be John Secret", () => {
 // toBe just checks that a value is what you expect. It uses === to check strict equality.
 // Use .toEqual when you want to check that two objects have the same value.
 
+// Less than and greater than
+test('Should be under 1600', () => {
+  const load1 = 800
+  const load2 = 700
+  expect(load1 + load2).toBeLessThan(1600)
+})
 
-
-
-
-// STOP IN 16:26

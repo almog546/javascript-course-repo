d038951fad7f8b63f34a4693dfee8d8e3f9782c5
John <emailjohnthomascaballero@gmail.com>
2023-11-25 21:18:07 +0800

    Essential javascript concepts Day 40

--- essential-javascript-concepts/18-performance-optimization.js
+++ essential-javascript-concepts/18-performance-optimization.js
@@ -17,5 +17,23 @@
 // This reduces the file size by about 30%.
 
 // Compression: Compression involves using algorithms to reduce the size of files. There are a variety of compression algorithms available, but some of the most common for web files are GZIP and Brotli.
+
 // Compression can reduce the size of files by up to 70%.
 
+// Lazy loading resources
+
+// Lazy loading is a technique for loading resources, such as images and JavaScript files, on demand. This means that the resources are not loaded until they are needed, which can improve the performance of your website.
+
+// There are a few different ways to implement lazy loading. One common method is to use JavaScript to load resources when they scroll into view.
+
+// Asynchronous loading of scripts
+
+// Asynchronous loading of scripts is a technique for loading scripts without blocking the rendering of the page. This means that the scripts are loaded in the background, and the page will continue to render while they are loading.
+
+// Asynchronous loading of scripts can improve the performance of your website by reducing the amount of time it takes for the page to load.
+
+// Here is an example of how to asynchronously load a script:
+
+{/* <script src="script.js" async></script> */}
+
+// This will load the script.js file asynchronously. The page will continue to render while the script is loading.

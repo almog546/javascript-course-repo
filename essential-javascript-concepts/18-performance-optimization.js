676217af7f95dfa5cc70b92fdd631e789816ff65
JOHN THOMAS F. CABALLERO <emailjohnthomascaballero@gmail.com>
2023-11-26 21:40:17 +0800

    Essential javascript concepts Day 41

--- essential-javascript-concepts/18-performance-optimization.js
+++ essential-javascript-concepts/18-performance-optimization.js
@@ -37,3 +37,29 @@
 {/* <script src="script.js" async></script> */}
 
 // This will load the script.js file asynchronously. The page will continue to render while the script is loading.
+
+
+
+
+
+
+
+
+// ADITIONAL SAMPLES:
+
+// Performance optimization is a crucial aspect of web development to ensure fast and efficient user experiences. Let's explore three key techniques for optimizing website performance: minification and compression, lazy loading resources, and asynchronous loading of scripts.
+
+// 1. Minification and Compression:
+// Minification:
+// Minification involves removing unnecessary characters from code without affecting its functionality. This reduces file sizes, leading to faster downloads and improved performance.
+
+// For example, consider the following JavaScript code:
+
+// function calculateTotal(price, quantity) {
+//  var result = price * quantity;
+//  return result;
+// }
+
+//After minification:
+
+// function calculateTotal(a,b){return a*b}

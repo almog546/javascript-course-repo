50c83766f768426c46c3b06c010c768ab417cffc
John <emailjohnthomascaballero@gmail.com>
2023-12-17 20:38:09 +0800

    Essential javascript concepts Day 54

---
+++ essential-javascript-concepts/21-version-control.js
@@ -0,0 +1,11 @@
+// ersion Control with Git: A Beginner's Guide
+// Since you've already explored using Git for code versioning, let's delve deeper into some core concepts and commands to solidify your understanding.
+
+// Git Basics:
+
+// Repository (repo): A storage space for your tracked code changes.
+// Version: A snapshot of your code at a specific point in time.
+// Commit: A recorded set of changes made to your code.
+// Branch: A separate line of development within your repository.
+// Remote: A hosted location for your repository (e.g., GitHub).
+

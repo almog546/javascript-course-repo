b7bab640b23688fc39d5abb27ac5b0bdc347aed3
John <emailjohnthomascaballero@gmail.com>
2023-12-17 20:56:29 +0800

    Essential javascript concepts Day 54

--- essential-javascript-concepts/21-version-control.js
+++ essential-javascript-concepts/21-version-control.js
@@ -9,3 +9,65 @@
 // Branch: A separate line of development within your repository.
 // Remote: A hosted location for your repository (e.g., GitHub).
 
+// Essential Git Commands:
+
+// git init: Initializes a Git repository in a directory.
+// git add <file>: Adds a file to the staging area for the next commit.
+// git commit -m "<message>": Records the changes in the staging area as a commit with a message.
+// git status: Shows the current state of your repository (tracked/untracked files, changes).
+// git log: Shows a history of commits made to the repository.
+// git checkout <branch>: Switches to a specific branch in your repository.
+// git clone <url>: Downloads a copy of a remote repository to your local machine.
+// git push <remote> <branch>: Pushes your local branch changes to the remote repository.
+// git pull <remote> <branch>: Pulls changes from the remote repository to your local branch.
+
+// Branching and Merging:
+
+// Branches allow you to work on different features or bug fixes without affecting the main codebase.
+// Merging combines changes from different branches into the main branch.
+
+// Benefits of Git:
+
+// Track code changes: Easily see what changed and when.
+// Revert mistakes: Rollback to a previous version if needed.
+// Collaboration: Work with others on the same codebase simultaneously.
+// Version control history: Understand how your code evolved over time.
+
+// Getting started:
+
+// Install Git on your computer.
+// Create a Git repository for your project.
+// Start making commits to track your changes.
+// Experiment with branching and merging to explore version control workflows.
+
+// Remember, practice makes perfect! The more you use Git, the more comfortable you'll become with its capabilities. Feel free to ask if you have any questions about specific commands or concepts.
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Great! Git is a powerful version control system widely used in software development to track changes, collaborate with others, and manage codebase versions. Let's cover the basics of using Git.
+
+// What is Git?
+// Git is a distributed version control system that allows multiple developers to work on a project simultaneously. It tracks changes to files over time, making it easy to collaborate, revert to previous states, and manage different versions of a codebase.
+
+// Common Git Commands:
+// git status: Shows the status of your working directory.
+// git add: Stages changes for commit.
+// git commit: Commits changes to the repository.
+// git log: Displays commit history.
+// git branch: Lists branches in your repository.
+// git checkout (or git switch): Switches branches.
+// git merge: Merges changes from one branch into another.
+// git pull: Fetches changes from a remote repository.
+// git push: Pushes changes to a remote repository.
+
+// With these basics, you have a solid foundation for using Git in your projects. As you become more comfortable with Git, you can explore more advanced features and workflows to enhance your version control practices.
---
+++ essential-javascript-concepts/22-code-quality-eslint.js
@@ -0,0 +1,44 @@
+// 1. Prettier:
+
+// What it is: Prettier is an opinionated code formatter that automatically formats your code according to its pre-defined rules. It aims to ensure consistent and visually appealing code across your project.
+// Benefits:
+// Saves time and effort on manual formatting.
+// Improves code readability and consistency.
+// Reduces code review disagreements.
+// Supports various programming languages and file types.
+// Getting started:
+// Install Prettier using your package manager (e.g., npm install -g prettier).
+// Configure Prettier for your preferred style using a .prettierrc file.
+// Integrate Prettier with your editor/IDE for real-time formatting.
+
+// 2. Prettier:
+
+// What it is: Prettier is an opinionated code formatter that automatically formats your code according to its pre-defined rules. It aims to ensure consistent and visually appealing code across your project.
+// Benefits:
+// Saves time and effort on manual formatting.
+// Improves code readability and consistency.
+// Reduces code review disagreements.
+// Supports various programming languages and file types.
+// Getting started:
+// Install Prettier using your package manager (e.g., npm install -g prettier).
+// Configure Prettier for your preferred style using a .prettierrc file.
+// Integrate Prettier with your editor/IDE for real-time formatting.
+
+// Remember: Both Prettier and code linting play crucial roles in maintaining high-quality code. You can utilize Prettier for consistent formatting and code linting for analyzing and improving code quality further.
+
+
+
+
+
+
+
+
+// Code Linting with ESLint:
+// What is ESLint?
+// ESLint is a widely used linting tool for JavaScript and TypeScript. It helps identify and fix common coding issues, enforces coding standards, and improves overall code quality.
+
+// Code Formatting with Prettier:
+// What is Prettier?
+// Prettier is a code formatter that automatically formats your code, ensuring a consistent and aesthetically pleasing style across your project.
+
+// With ESLint and Prettier configured, you have a powerful combination for maintaining code quality and consistency in your projects. These tools help catch errors, enforce coding standards, and ensure that your codebase is clean and readable.

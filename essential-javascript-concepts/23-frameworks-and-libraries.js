b25a2464bee740e56ba3d3cd90864a229cae0377
John <emailjohnthomascaballero@gmail.com>
2023-12-19 20:12:38 +0800

    Essential javascript concepts Day 55

--- essential-javascript-concepts/23-frameworks-and-libraries.js
+++ essential-javascript-concepts/23-frameworks-and-libraries.js
@@ -24,3 +24,73 @@
 
 
 
+
+
+
+// Great! Let's dive into a brief introduction to three popular JavaScript libraries and frameworks: React, Angular, and Vue.
+
+// React:
+// What is React?
+// React is a JavaScript library for building user interfaces, particularly for single-page applications where the UI needs to be highly dynamic and responsive. It's developed and maintained by Facebook.
+
+// Key Concepts:
+
+// 1. Components:
+
+// React applications are built using components, which are reusable and independent pieces of UI.
+
+// 2. Virtual DOM:
+
+// React uses a virtual DOM to efficiently update the actual DOM. It calculates the most efficient way to update the UI and applies the changes only where necessary.
+
+// 3. JSX:
+
+// JSX (JavaScript XML) is a syntax extension for JavaScript that looks similar to XML or HTML. It allows you to write HTML elements and components in your JavaScript code.
+
+// 4. sProps and State:
+
+// Props are used to pass data from parent components to child components. State represents the internal state of a component.
+
+// Angular:
+// What is Angular?
+// Angular is a TypeScript-based open-source framework for building web applications. It's maintained by Google and provides a comprehensive solution for developing large-scale and maintainable applications.
+
+// Key Concepts:
+
+// 1. Modules:
+
+// Angular applications are organized into modules, which are containers for components, services, and other features.
+
+// 2. Components:
+
+// Components are the basic building blocks of an Angular application. Each component has its own template, style, and behavior.
+
+// 3. Services:
+
+// Services are used for encapsulating reusable business logic and data.
+
+// 4. Dependency Injection:
+
+// Angular uses dependency injection to manage and organize components and services.
+
+// Vue:
+// What is Vue?
+// Vue.js is a progressive JavaScript framework for building user interfaces. It's designed to be incrementally adoptable and can be integrated into existing projects.
+
+// Key Concepts:
+
+// 1. Components:
+
+// Vue.js applications are also built using components, similar to React. Components encapsulate templates, scripts, and styles.
+
+// 2. Vue Instance:
+
+// Vue.js applications start with a root Vue instance that controls the entire application.
+
+// 3. Directives:
+
+// Vue uses directives to apply special behavior to elements. For example, v-if for conditional rendering and v-for for iterating over a list.
+
+// 4. Two-Way Data Binding:
+
+// Vue supports two-way data binding between the view and the model, making it easy to keep the UI in sync with the underlying data.

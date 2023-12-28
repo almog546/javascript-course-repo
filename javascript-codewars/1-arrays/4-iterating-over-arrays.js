0da958f90c347ef49fcff909b5afa5de88ed5523
John <emailjohnthomascaballero@gmail.com>
2023-12-28 18:24:43 +0800

    JavaScript Codewars Day 7

--- javascript-codewars/1-arrays/4-iterating-over-arrays.js
+++ javascript-codewars/1-arrays/4-iterating-over-arrays.js
@@ -6,9 +6,15 @@ for (let i = 0; i < names.length; i++) {
 }
 
 // For...of loop:
-let games = ['assassin creed syndicate', 'fortnite', 'call of duty mobile']
+let games = ["assassin creed syndicate", "fortnite", "call of duty mobile"];
 
 for (const name of games) {
   console.log(name);
 }
 
+// forEach method:
+let movies = ["John Wick", "Spider Man", "Samaritan", "Deadpool"];
+
+movies.forEach(function (movie) {
+  console.log(movie);
+});
---
+++ javascript-codewars/1-arrays/5-searching-arrays.js
@@ -0,0 +1,11 @@
+let social = ['facebook', 'twitter', 'WhatsApp', 'LinkedIn']
+
+// indexOf (find first occurrence):
+let foundIndex = social.indexOf('facebook')
+console.log(foundIndex)
+
+// lastIndexOf (find last occurrence):
+let lastLinkedInIndex = social.lastIndexOf('LinkedIn')
+console.log(lastLinkedInIndex)
+
+// Remember to practice regularly and read the documentation for more methods and techniques!

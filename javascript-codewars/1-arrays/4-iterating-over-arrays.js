5aee34ab2eb67e24270e3950c52b7f7689efc230
John <emailjohnthomascaballero@gmail.com>
2023-12-28 17:58:07 +0800

    JavaScript Codewars Day 6

--- javascript-codewars/1-arrays/4-iterating-over-arrays.js
+++ javascript-codewars/1-arrays/4-iterating-over-arrays.js
@@ -4,3 +4,11 @@ let names = ["John", "Lee", "Paul", "Rose"];
 for (let i = 0; i < names.length; i++) {
   console.log(names[i]);
 }
+
+// For...of loop:
+let games = ['assassin creed syndicate', 'fortnite', 'call of duty mobile']
+
+for (const name of games) {
+  console.log(name);
+}
+
